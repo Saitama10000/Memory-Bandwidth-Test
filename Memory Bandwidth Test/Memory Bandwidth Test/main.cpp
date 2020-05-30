31db7506679a429844ea788e60215199cd69df4e
Saitama10000 <62643420+Saitama10000@users.noreply.github.com>
2020-05-30 14:10:47 +0300

    edited for readability

--- Memory Bandwidth Test/Memory Bandwidth Test/main.cpp
+++ Memory Bandwidth Test/Memory Bandwidth Test/main.cpp
@@ -23,26 +23,29 @@ int main()
 		unsigned long long end = 0;
 		unsigned long long duration = 0;
 		unsigned long long cycles = 0;
-		long long n = 0;
+		unsigned long long n = 0;
 
 		for (int i = 0; i < 1000; i++)
 		{
 			start = cycle_start();
 			read(input, size);
 			end = cycle_end();
-			duration = end - start;
 
+			duration = end - start;
 			cycles += duration; n += 1;
 
 		}
 
-		volatile double bandwidth = ((double)size) * 4.0 / ((double)(cycles / n-20)) * 4.0;
+		const double cpuFrequency = 4.0;
+		const double peakBandwidth = 42.666;
+		double bandwidth = (double)(size * sizeof(int)) / (double)(cycles / n - 20) * cpuFrequency;
+
 		printf("%15.2e %16lld %16lld %16.2f GB/s  %2.3f%% peak\n",
 				(double)(cycles / n),
 				cycles / n,
 				duration,
 				bandwidth,
-				bandwidth/.4266666
+				bandwidth / peakBandwidth * 100.0
 		);
 	}
 	
